0663f492c1907c8f67f4040c7a021b41928aae54
Yienisha Abeyratne <yienishaedu@gmail.com>
2025-02-26 00:08:52 -0500

    t

---
+++ src/main/java/com/yorku/green_charge_auto/GreenChargeAutoApplication.java
@@ -0,0 +1,13 @@
+package com.yorku.green_charge_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenChargeAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenChargeAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yorku/green_charge_auto/GreenChargeAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorku.green_charge_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenChargeAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
