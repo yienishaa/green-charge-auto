eb98e86688c72864ad8a9c146d1d51a9668fecc8
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-03-21 15:25:21 -0400

    Update VehicleRepository.java

--- src/main/java/com/yorku/green_charge_auto/repository/VehicleRepository.java
+++ src/main/java/com/yorku/green_charge_auto/repository/VehicleRepository.java
@@ -4,6 +4,11 @@ import com.yorku.green_charge_auto.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+    Optional<Vehicle> findByName(String name);
+    List<Vehicle> findByBrand(String brand);
 }
