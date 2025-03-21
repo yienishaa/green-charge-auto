1480636a79c7f44491448312c7750ea80609c5bb
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-03-21 15:27:47 -0400

    Update VehicleService.java

--- src/main/java/com/yorku/green_charge_auto/service/VehicleService.java
+++ src/main/java/com/yorku/green_charge_auto/service/VehicleService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.Optional;
 
 @Service
 public class VehicleService {
@@ -14,19 +14,39 @@ public class VehicleService {
     @Autowired
     private VehicleRepository vehicleRepository;
 
-    private VehicleService(VehicleRepository vehicleRepository) {
-        this.vehicleRepository = vehicleRepository;
-    }
-
     public List<Vehicle> getAllVehicles() {
         return vehicleRepository.findAll();
     }
 
-    public Vehicle getVehicleById(int id) {
-        return vehicleRepository.findById(id).get();
+    public Optional<Vehicle> getVehicleById(int id) {
+        return vehicleRepository.findById(id);
     }
 
-    public Vehicle getVehicleByName(String vehicleName) {
-        return vehicleRepository.findByName(vehicleName);
+    public Optional<Vehicle> getVehicleByName(String name) {
+        return vehicleRepository.findByName(name);
+    }
+
+    public List<Vehicle> getVehiclesByBrand(String brand) {
+        return vehicleRepository.findByBrand(brand);
+    }
+
+    public Vehicle addVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    public Vehicle updateVehicle(int id, Vehicle updatedVehicle) {
+        return vehicleRepository.findById(id).map(vehicle -> {
+            vehicle.setName(updatedVehicle.getName());
+            vehicle.setDescription(updatedVehicle.getDescription());
+            vehicle.setBrand(updatedVehicle.getBrand());
+            vehicle.setModel(updatedVehicle.getModel());
+            vehicle.setPrice(updatedVehicle.getPrice());
+            vehicle.setQuantity(updatedVehicle.getQuantity());
+            return vehicleRepository.save(vehicle);
+        }).orElseThrow(() -> new RuntimeException("Vehicle not found with id " + id));
+    }
+
+    public void deleteVehicle(int id) {
+        vehicleRepository.deleteById(id);
     }
 }
