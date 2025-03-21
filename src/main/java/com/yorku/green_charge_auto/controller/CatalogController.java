8d96397e72d3e83e9f961ab9ce61a434c5aec925
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-03-21 15:26:20 -0400

    Update CatalogController.java

--- src/main/java/com/yorku/green_charge_auto/controller/CatalogController.java
+++ src/main/java/com/yorku/green_charge_auto/controller/CatalogController.java
@@ -1,9 +1,55 @@
 package com.yorku.green_charge_auto.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.yorku.green_charge_auto.model.Vehicle;
+import com.yorku.green_charge_auto.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/vehicles")
 public class CatalogController {
 
+    @Autowired
+    private VehicleService vehicleService;
 
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable int id) {
+        return vehicleService.getVehicleById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/brand/{brand}")
+    public List<Vehicle> getVehiclesByBrand(@PathVariable String brand) {
+        return vehicleService.getVehiclesByBrand(brand);
+    }
+
+    @PostMapping
+    public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.addVehicle(vehicle);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Vehicle> updateVehicle(@PathVariable int id, @RequestBody Vehicle updatedVehicle) {
+        try {
+            return ResponseEntity.ok(vehicleService.updateVehicle(id, updatedVehicle));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVehicle(@PathVariable int id) {
+        vehicleService.deleteVehicle(id);
+        return ResponseEntity.noContent().build();
+    }
 }
