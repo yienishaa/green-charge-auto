89d1bbfe178afc6f895e773b255ce327f5d954e1
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-03-21 16:42:43 -0400

    Create JwtUtil.java

---
+++ src/main/java/com/yorku/green_charge_auto/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.yorku.green_charge_auto.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private String SECRET_KEY = "secret";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean validateToken(String token, String username) {
+        return (extractUsername(token).equals(username) && !extractExpiration(token).before(new Date()));
+    }
+}
