29163f15bd7b7d9c3d6402c5f41ec47c290a6fee
Kyle Truong <kyle.trng@gmail.com>
2025-03-27 16:41:27 -0400

    Add: basic chat functionality

--- src/main/java/com/yorku/green_charge_auto/config/SecurityConfig.java
+++ src/main/java/com/yorku/green_charge_auto/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/vehicles/**", "/orders/**", "/shopping-cart/**","/loan-calculator/**", "/reviews/**").permitAll()
+                        .requestMatchers("/vehicles/**", "/orders/**", "/shopping-cart/**","/loan-calculator/**", "/reviews/**", "/chatbot/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
