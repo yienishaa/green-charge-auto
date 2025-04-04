e92d404579ad525282d36326cdb8abc17a8b4aeb
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-04-04 02:55:07 -0400

    Update ReviewController.java

--- src/main/java/com/yorku/green_charge_auto/controller/ReviewController.java
+++ src/main/java/com/yorku/green_charge_auto/controller/ReviewController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
