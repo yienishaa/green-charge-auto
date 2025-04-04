f6946e91ecf720fd68c56cb95721dc4fc973088a
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-04-04 02:55:27 -0400

    Update ShoppingCartController.java

--- src/main/java/com/yorku/green_charge_auto/controller/ShoppingCartController.java
+++ src/main/java/com/yorku/green_charge_auto/controller/ShoppingCartController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/shopping-cart")
 public class ShoppingCartController {
