bba698df2550c779068db07cc180b3fa687cbc2e
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-04-04 16:44:10 -0400

    Update CorsConfig.java

--- src/main/java/com/yorku/green_charge_auto/config/CorsConfig.java
+++ src/main/java/com/yorku/green_charge_auto/config/CorsConfig.java
@@ -1,27 +1,27 @@
-// package com.yorku.green_charge_auto.config;
+package com.yorku.green_charge_auto.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig {
+@Configuration
+public class CorsConfig {
 
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins(
-//                             "http://localhost:3000",
-//                             "http://greenchargetest.s3-website-us-east-1.amazonaws.com"
-//                         )
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                         .allowedHeaders("*")
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "http://greenchargetest.s3-website-us-east-1.amazonaws.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
