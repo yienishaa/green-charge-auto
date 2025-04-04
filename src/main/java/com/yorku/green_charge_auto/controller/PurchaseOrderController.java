edf12dd89a6fb9462d657be34d64af6d9437fc77
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-04-04 02:54:56 -0400

    Update PurchaseOrderController.java

--- src/main/java/com/yorku/green_charge_auto/controller/PurchaseOrderController.java
+++ src/main/java/com/yorku/green_charge_auto/controller/PurchaseOrderController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/orders")
 public class PurchaseOrderController {
