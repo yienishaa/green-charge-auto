03e8735a17147bf72ec6cea76b266ad35f5d3c9e
Yienisha Abeyratne <yienishaedu@gmail.com>
2025-04-05 01:25:34 -0400

    Login changes

--- src/main/java/com/yorku/green_charge_auto/config/WebConfig.java
+++ src/main/java/com/yorku/green_charge_auto/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.yorku.green_charge_auto.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
