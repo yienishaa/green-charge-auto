c08563d5d95e45cf9b0441dac9ecb30495364e09
Aryan Chaudhary <98985506+Lep15@users.noreply.github.com>
2025-04-04 23:33:11 -0400

    Update AuthController.java

--- src/main/java/com/yorku/green_charge_auto/controller/AuthController.java
+++ src/main/java/com/yorku/green_charge_auto/controller/AuthController.java
@@ -3,13 +3,16 @@ package com.yorku.green_charge_auto.controller;
 
 import com.yorku.green_charge_auto.constants.Role;
 import com.yorku.green_charge_auto.service.AuthService;
+import com.yorku.green_charge_auto.service.ShoppingCartService;
+
 import lombok.Data;
 import com.yorku.green_charge_auto.dto.LoginRequest;
+import com.yorku.green_charge_auto.dto.LoginResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -17,18 +20,27 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+    
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> registerUser(@RequestBody RegisterRequest request) {
-        String token = authService.registerUser(request.getUsername(), request.getPassword(), request.getRole());
-        return ResponseEntity.ok(Map.of("token", token));
+    public String register(@RequestBody LoginRequest request) {
+        return authService.registerUser(request.getUsername(), request.getPassword(), Role.USER);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> loginUser(@RequestBody LoginRequest loginRequest) throws Exception {
-        String token = authService.loginUser(loginRequest);
-        return ResponseEntity.ok(Map.of("token", token));
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.loginUser(loginRequest);
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@RequestParam int userId) {
+        shoppingCartService.clearCartByUserId(userId);
+        return ResponseEntity.ok("Cart cleared and user logged out.");
+    }
+
 
     @Data
     static class RegisterRequest {
